f6f9aabd24559aa8e4286a468fc4e912cc3127d8
Ujjwal Kumar <ujjwalkumar8577@gmail.com>
2021-04-10 00:24:23 +0530

    Deleted reduntant buttons

--- app/src/main/java/com/ujjwalkumar/easybiz/MenuActivity.java
+++ app/src/main/java/com/ujjwalkumar/easybiz/MenuActivity.java
@@ -10,7 +10,7 @@ import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
-    Button button1,button2,button3,button4,button5,button6,button7,button8;
+    Button button1,button2,button3,button5,button6;
     private Intent in = new Intent();
 
     @Override
@@ -21,11 +21,8 @@ public class MenuActivity extends AppCompatActivity {
         button1 = findViewById(R.id.button1);
         button2 = findViewById(R.id.button2);
         button3 = findViewById(R.id.button3);
-        button4 = findViewById(R.id.button4);
         button5 = findViewById(R.id.button5);
         button6 = findViewById(R.id.button6);
-        button7 = findViewById(R.id.button7);
-        button8 = findViewById(R.id.button8);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,16 +54,6 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                in.setAction(Intent.ACTION_VIEW);
-                in.setClass(getApplicationContext(), MyAccountActivity.class);
-                in.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                startActivity(in);
-            }
-        });
-
         button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -82,26 +69,6 @@ public class MenuActivity extends AppCompatActivity {
             public void onClick(View view) {
                 in.setAction(Intent.ACTION_VIEW);
                 in.setClass(getApplicationContext(), StaffActivity.class);
-                in.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                startActivity(in);
-            }
-        });
-
-        button7.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                in.setAction(Intent.ACTION_VIEW);
-                in.setClass(getApplicationContext(), CashCalculatorActivity.class);
-                in.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-                startActivity(in);
-            }
-        });
-
-        button8.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                in.setAction(Intent.ACTION_VIEW);
-                in.setClass(getApplicationContext(), ProductActivity.class);
                 in.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
                 startActivity(in);
             }
