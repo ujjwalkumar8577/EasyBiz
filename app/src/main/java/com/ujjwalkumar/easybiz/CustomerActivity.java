8c56ad5b0b2375312bc01a727d656d8a5205efbf
Ujjwal Kumar <ujjwalkumar8577@gmail.com>
2021-04-11 03:00:29 +0530

    Working CustomerActivity

--- app/src/main/java/com/ujjwalkumar/easybiz/CustomerActivity.java
+++ app/src/main/java/com/ujjwalkumar/easybiz/CustomerActivity.java
@@ -2,13 +2,161 @@ package com.ujjwalkumar.easybiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CustomerActivity extends AppCompatActivity {
+
+    private HashMap<String, String> mp = new HashMap<>();
+    private ArrayList<HashMap<String, String>> filtered = new ArrayList<>();
+
+    private ImageView backBtn;
+    private ListView listviewCustomer;
+
+    private FirebaseDatabase fbdb = FirebaseDatabase.getInstance();
+    private DatabaseReference dbref = fbdb.getReference("customers");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
+
+        backBtn = findViewById(R.id.backBtn);
+        listviewCustomer = findViewById(R.id.listviewCustomer);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent();
+                in.setAction(Intent.ACTION_VIEW);
+                in.setClass(getApplicationContext(),Dashboard.class);
+                in.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(in);
+                finish();
+            }
+        });
+
+        loadList();
     }
+
+    private void loadList() {
+        dbref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                filtered = new ArrayList<>();
+                try {
+                    GenericTypeIndicator<HashMap<String, String>> ind = new GenericTypeIndicator<HashMap<String, String>>() {
+                    };
+                    for (DataSnapshot data : dataSnapshot.getChildren()) {
+                        HashMap<String, String> map = data.getValue(ind);
+                        filtered.add(map);
+                    }
+                    listviewCustomer.setAdapter(new CustomerActivity.ListviewCustomerAdapter(filtered));
+                    ((BaseAdapter)listviewCustomer.getAdapter()).notifyDataSetChanged();
+                }
+                catch (Exception e) {
+                    Toast.makeText(CustomerActivity.this, "An exception occured", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError _databaseError) {
+            }
+        });
+    }
+
+    public class ListviewCustomerAdapter extends BaseAdapter {
+        ArrayList<HashMap<String, String>> data;
+
+        public ListviewCustomerAdapter(ArrayList<HashMap<String, String>> arr) {
+            data = arr;
+        }
+
+        @Override
+        public int getCount() {
+            return data.size();
+        }
+
+        @Override
+        public HashMap<String, String> getItem(int index) {
+            return data.get(index);
+        }
+
+        @Override
+        public long getItemId(int index) {
+            return index;
+        }
+
+        @Override
+        public View getView(final int position, View view, ViewGroup viewGroup) {
+            LayoutInflater inflater = (LayoutInflater) getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View v = view;
+            if (v == null) {
+                v = inflater.inflate(R.layout.customers, null);
+            }
+
+            final TextView textview1 = (TextView) v.findViewById(R.id.textview1);
+            final TextView textview2 = (TextView) v.findViewById(R.id.textview2);
+            final TextView textview3 = (TextView) v.findViewById(R.id.textview3);
+            final ImageView imageviewCall = (ImageView) v.findViewById(R.id.imageviewCall);
+            final ImageView imageview1Dir = (ImageView) v.findViewById(R.id.imageviewDir);
+
+            double lat = Double.parseDouble(filtered.get(position).get("lat").toString());
+            double lng = Double.parseDouble(filtered.get(position).get("lng").toString());
+
+            textview1.setText(filtered.get(position).get("name").toString());
+            textview2.setText(filtered.get(position).get("contact").toString());
+            textview3.setText(filtered.get(position).get("area").toString());
+
+            imageviewCall.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View _view) {
+                    Intent inv = new Intent();
+                    inv.setAction(Intent.ACTION_CALL);
+                    inv.setData(Uri.parse("tel:".concat(filtered.get(position).get("contact").toString())));
+                    startActivity(inv);
+                }
+            });
+            imageview1Dir.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View _view) {
+                    Intent inv = new Intent();
+                    inv.setAction(Intent.ACTION_VIEW);
+                    inv.setData(Uri.parse("google.navigation:q=".concat(String.valueOf(lat).concat(",".concat(String.valueOf(lng))))));
+                    if(inv.resolveActivity(getPackageManager())!=null) {
+                        startActivity(inv);
+                    }
+                    else
+                    {
+                        Toast.makeText(CustomerActivity.this, "No app found for navigation", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            return v;
+        }
+    }
+
+
 }
